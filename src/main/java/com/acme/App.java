0e1e12ac171f177375b0e2a8ebf635a145d89871
Claudia Sonarova <claudia.sonarova@gmail.com>
2020-11-19 12:58:20 +0100

    add statement method

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -3,10 +3,16 @@ package com.acme;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+    app = new App();
+    System.out.println(app.getGreeting());
+    System.out.println(app.getStatement());
   }
 
   public String getGreeting() {
     return "Hello world.";
+  }
+
+  public String getStatement() {
+    return "The weather is fine.";
   }
 }
